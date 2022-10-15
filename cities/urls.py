4f010254c3a596be76b2a03a9281223f7d1aef09
Aleksandr <tuzovap@gmail.com>
2022-10-15 22:35:15 +0300

    Страница детализации по городу

--- cities/urls.py
+++ cities/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from cities.views import home
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('<int:pk>/', home, name='home')
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from cities.models import City
 
 
-def home(request):
+def home(request, pk=None):
+    if pk:
+        # city = City.objects.filter(id=pk).first()
+        city = get_object_or_404(City, id=pk)
+        content = {'object': city}
+        return render(request, 'cities/detail.html', content)
     qs = City.objects.all()
     content = {'objects_list': qs}
     return render(request, 'cities/home.html', content)
