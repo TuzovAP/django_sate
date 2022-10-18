ee49564e45fbd3a46309aa8393edb77b36223ce3
Aleksandr <tuzovap@gmail.com>
2022-10-18 23:00:31 +0300

    Создание новой записи через CreateView

--- cities/urls.py
+++ cities/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cities.views import home, CityDetailView
+from cities.views import home, CityDetailView, CityCreateView
 
 urlpatterns = [
     path('', home, name='home'),
-    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail')
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('add/', CityCreateView.as_view(), name='create')
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
 
 from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 
+# функция для прорисовки страницы
 def home(request, pk=None):
     if request.method == 'POST':
         form = CityForm(request.POST)
@@ -25,3 +27,10 @@ def home(request, pk=None):
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City  # таблица городов в БД
+    form_class = CityForm  # модель формы через django
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')  # в случае успеха переходим на cities:home, но нужно испольховать ленивый реверс
