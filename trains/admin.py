cb11b930d5c4c5d2888858f594447b43ccea23ba
Aleksandr <tuzovap@gmail.com>
2022-10-27 22:20:42 +0300

    Детализация отобрадения списка в админке на сайте

--- trains/admin.py
+++ trains/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from trains.models import Train
 
 
-admin.site.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    class Meta():
+        model = Train
+    # список для отображения в админке на сайте
+    list_display = ('name', 'from_city', 'to_city', 'travel_time')
+    # что можно редактировать прямо в общей таблице в админке на сайте
+    list_editable = ('travel_time',)
+
+# регистрирую классы
+admin.site.register(Train, TrainAdmin)
