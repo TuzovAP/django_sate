de225513cc58509284bc007b6f3c81504711a100
Aleksandr <tuzovap@gmail.com>
2022-11-01 22:08:14 +0300

    Подключение отдельной функции для поиска маршрутов

--- routes/views.py
+++ routes/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render
 from routes.forms import RouteForm
+from routes.utils import get_routes
 
 
 # функция для прорисовки главной страницы приложения
@@ -12,7 +13,13 @@ def home(request):
 def find_routes(request):
     if request.method == 'POST':    # если форма заполнена через сайт
         form = RouteForm(request.POST)
-        a = 1
+        if form.is_valid():
+            try:  # функция может отрабатывать с ошибками
+                context = get_routes(request, form)
+            except ValueError as ex:
+                messages.error(request, ex)
+                return render(request, 'routes/home.html', {'form': form})
+            return render(request, 'routes/home.html', context)
         return render(request, 'routes/home.html', {'form': form})
     else:                           # если пользователь самойстоятельно указал адрес через браузер
         form = RouteForm()
