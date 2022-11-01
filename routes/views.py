76e68b3c36f1175a51994c66553728a717351fab
Aleksandr <tuzovap@gmail.com>
2022-11-01 21:48:55 +0300

    Добавил функцию обработки данных из формы для поиска

--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from routes.forms import RouteForm
 
@@ -6,3 +7,14 @@ from routes.forms import RouteForm
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
+
+# функция для отработки запросов через форму поиска маршрута на сайте
+def find_routes(request):
+    if request.method == 'POST':    # если форма заполнена через сайт
+        form = RouteForm(request.POST)
+        a = 1
+        return render(request, 'routes/home.html', {'form': form})
+    else:                           # если пользователь самойстоятельно указал адрес через браузер
+        form = RouteForm()
+        messages.error(request, 'Нет данных для поиска')
+        return render(request, 'routes/home.html', {'form': form})  # отправляю на основную страницу с формой
