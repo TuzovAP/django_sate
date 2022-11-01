e2a6a5ed825ff9ad4868a4257ad9d742990e79a2
Aleksandr <tuzovap@gmail.com>
2022-11-01 20:34:25 +0300

    Добавил SELECT2 для красивого отображения формы поиска маршрута на сайте

--- routes/forms.py
+++ routes/forms.py
@@ -6,18 +6,18 @@ class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(label='Откуда',  # ModelChoiceField - выпадающий список
                                        queryset=City.objects.all(),  # City.objects.all()) - отбор всех записей
                                        widget=forms.Select(  # Виджит для выбора
-                                           attrs={'class': 'form-control'}  # красивое отображение для Bootstrap
+                                           attrs={'class': 'form-control js-example-basic-single'}  # красивое отображение для Bootstrap
                                        ))
     to_city = forms.ModelChoiceField(label='Куда',  # ModelChoiceField - выпадающий список
                                        queryset=City.objects.all(),  # City.objects.all()) - отбор всех записей
                                        widget=forms.Select(  # Виджит для выбора
-                                           attrs={'class': 'form-control'}  # красивое отображение для Bootstrap
+                                           attrs={'class': 'form-control js-example-basic-single'}  # красивое отображение для Bootstrap
                                        ))
     # список городов через которые проложить маршрут
     cities =forms.ModelMultipleChoiceField(  # ModelMultipleChoiceField модель для выбора нескольких значений
         label='Через города', queryset=City.objects.all(),
         required=False,  # required - обязательное поле?
-        widget=forms.SelectMultiple(attrs={'class': 'form-control'})
+        widget=forms.SelectMultiple(attrs={'class': 'form-control js-example-basic-multiple'})
     )
     travelling_time = forms.IntegerField(label='Время в пути', widget=forms.NumberInput(attrs={  # виджет для отрисовки формы
         'class': 'form-control',  # красивое отображение для Bootstrap
